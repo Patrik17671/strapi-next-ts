013baa3ef3b316a431b3cbb40414b72cc40b39ed
Patrik17671 <patrik.spilka@gmail.com>
2023-01-31 10:47:31 +0100

    redeploy test

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import type {AppProps} from 'next/app'
 import '../styles/main.sass'
 import {Provider as ProviderApi, createClient} from "urql";
 import Nav from "../components/header/Nav";
-import {store} from '../redux/store';
+import {store} from '@/redux/store';
 import { Provider } from 'react-redux';
 import {Toaster} from 'react-hot-toast';
 
